51f464e1010dc1a1d5c3507f2b5265bc12f3ed33
Andy <andy@workshopshed.com>
2015-04-27 12:51:59 +0100

    Removed callback

--- Code/Python/GetWeather.py
+++ Code/Python/GetWeather.py
@@ -5,7 +5,7 @@ print 'Getting Weather'
 #Chicago,Il = 2379574
 
 shortweather_url = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Chicago%20IL%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-weather_url = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%3D2379574%20and%20u%3D'c'%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+weather_url = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%3D2379574%20and%20u%3D'c'%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 r = requests.get(weather_url)
 
 print r.status_code
