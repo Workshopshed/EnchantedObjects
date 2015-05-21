aaa3244615ed93821a796a4de415b73b1120284f
Andy <andy@workshopshed.com>
2015-05-21 21:08:30 +0100

    Added delays

--- Code/PowerTest2/PowerTest2.ino
+++ Code/PowerTest2/PowerTest2.ino
@@ -7,18 +7,19 @@ void setup()
 {
   pinMode(ledPin, OUTPUT);      // sets the digital pin as output
   pinMode(A5, OUTPUT);
+  delay(25000);                 // put in a start up delay to make it easier to re-programme
 }
 
 void loop()
 {
   digitalWrite(A5, HIGH);    // sets the Linino on
   digitalWrite(ledPin, LOW);    // sets the LED off
-  delay(5000);
+  delay(8000);
 
   digitalWrite(A5, LOW);   // sets the Linino off
   digitalWrite(ledPin, HIGH);   // sets the LED on
 
-  delay(5000);                  // waits for a second
+  delay(8000);                  // waits for a second
   digitalWrite(ledPin, LOW);   // both lights off, power and P13
   
   //Sleep
