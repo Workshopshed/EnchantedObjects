6210e145a73160db5ff2b4909682750fdb0d6a26
Andy <andy@workshopshed.com>
2015-05-22 08:02:54 +0100

    Tidy up of old code

--- Code/Python/curltest.py
+++ Code/Python/curltest.py
@@ -3,9 +3,7 @@ from StringIO import StringIO
 
 buffer = StringIO()
 c = pycurl.Curl()
-#c.setopt(c.URL, "https://priorityplant.com/");
 c.setopt(c.URL, "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%3D2379574%20and%20u%3D'c'%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
-# c.setopt(c.WRITEDATA, buffer)
 c.setopt(c.WRITEFUNCTION, buffer.write)
 c.perform()
 c.close()
