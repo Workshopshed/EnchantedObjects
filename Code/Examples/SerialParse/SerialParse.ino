ea2d79cb8651b8bdeeb9821b51e7301b049c51ab
Andy <andy@workshopshed.com>
2015-06-25 00:00:04 +0100

    Serial Parse
    Send and receive weather requests

--- Code/Examples/SerialParse/SerialParse.ino
+++ Code/Examples/SerialParse/SerialParse.ino
@@ -1,14 +1,23 @@
 //Test writing to serial
 void setup() {
   Serial.begin(115200);   //To PC
+  while (!Serial) {
+     ; // wait for serial port to connect.
+   }
+  
+  Serial1.begin(115200);   //To Linino
   float t = 25.4;
   float h = 78;
-  Serial.print("OK!,");
-  Serial.print(t);
-  Serial.print(",");
-  Serial.print(h);
-  Serial.print("\n");
+  Serial1.print("OK!,");
+  Serial1.print(t);
+  Serial1.print(",");
+  Serial1.print(h);
+  Serial1.print("\n");
 }
 
 void loop() {
+  if (Serial1.available() > 0) {
+    String weather = Serial1.readStringUntil('\n');
+    Serial.println(weather);
+  }
 }
